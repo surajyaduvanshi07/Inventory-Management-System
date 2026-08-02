679ad1a8455b7889e85384073de6549efb591d59
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-08-02 11:53:56 +0530

    Added Update Feature

--- database.py
+++ database.py
@@ -1,25 +1,21 @@
-import mysql.connector
+import sqlite3
 
-connection = mysql.connector.connect(
-    host = "mysql.railway.internal",
-    user = "root",
-    password = "jdaBUipZFCNnEmGidEScmarsFxZyissD",
-    database = "railway"
-)
+# Database Connection
+connection = sqlite3.connect("inventory.db", check_same_thread=False)
 
+# Cursor
 cursor = connection.cursor()
 
+print("Database Connected Successfully")
 
+# Create Product Table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Product(
-    product_id INT PRIMARY KEY,
-    product_name VARCHAR(100),
-    product_price FLOAT,
-    product_category VARCHAR(100)
+    product_id INTEGER PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    product_price REAL NOT NULL,
+    product_category TEXT NOT NULL
 )
 """)
 
 connection.commit()
-
-print("Database Connected Successfully")
-
