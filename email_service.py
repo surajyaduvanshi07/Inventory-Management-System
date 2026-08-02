efa39c7761366234f53d4d7b35e221c0a6c233b4
surajyaduvanshi07 <surajyaduvanshi0706@gmail.com>
2026-08-02 17:16:58 +0530

    Updated

--- email_service.py
+++ email_service.py
@@ -4,18 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-EMAIL = os.getenv("EMAIL")
-RESEND_API_KEY = os.getenv("RESEND_API_KEY")
-
-resend.api_key = RESEND_API_KEY
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 
 def send_otp(receiver_email, otp):
 
-    params = {
-        "from": f"Inventory Management <{EMAIL}>",
-        "to": [receiver_email],
+    resend.Emails.send({
+
+        "from": "onboarding@resend.dev",
+
+        "to": receiver_email,
+
         "subject": "Inventory Management OTP",
+
         "html": f"""
         <h2>Inventory Management System</h2>
 
@@ -23,10 +24,8 @@ def send_otp(receiver_email, otp):
 
         <h1>{otp}</h1>
 
-        <p>This OTP will expire in 5 minutes.</p>
+        <p>This OTP is valid for 5 minutes.</p>
 
         <p>Do not share this OTP with anyone.</p>
         """
-    }
-
-    resend.Emails.send(params)
+    })
